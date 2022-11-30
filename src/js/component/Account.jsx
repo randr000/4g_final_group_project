243a1a7759dbec6ee7f6cfad3831b5d34f81c774
Raul Andrial <rauljandrial@gmail.com>
2022-11-30 07:18:25 +0000

    fetch omdb search working

--- src/js/component/Account.jsx
+++ src/js/component/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext.jsx';
 
@@ -7,9 +7,22 @@ const Account = () => {
     const {user, logout} = UserAuth();
     const navigate = useNavigate();
 
+    const [movies, setMovies] = useState(false);
+
     useEffect(() => {
         console.log(`user: ${!!user}`);
     }, []);
+
+    const handleFetch = async () => {
+        try {
+            fetch(`https://www.omdbapi.com/?s=batman&apikey=${process.env.REACT_APP_OMDB}`)
+                .then(res => res.json())
+                .then(res => res['Response'] ? setMovies(res['Search']) : null);
+
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
 
     const handleLogout = async () => {
 
@@ -26,6 +39,10 @@ const Account = () => {
             Account:
             <p>User Email: {user && user.email}</p>
             <button onClick={handleLogout} className='btn btn-primary'>Logout</button>
+            <button onClick={handleFetch} className='btn btn-info'>Fetch</button>
+            <ul>
+                {movies ? movies.map(m => <p>{m.Title}</p>): null}
+            </ul>
         </div>
     );
 };
--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,3 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<BrowserRouter><Home /></BrowserRouter>, document.querySelector("#app"));
-
-// console.log(process.env.NODE_ENV);
-// console.log(process.env);
-// console.log(process.env.REACT_APP_TEST);
