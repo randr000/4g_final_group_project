e41db182c96b621cc1c8ad8b262ff96884e77314
Raul Andrial <rauljandrial@gmail.com>
2022-12-01 05:50:16 +0000

    Add to Favorites button changes when clicked

--- src/js/component/MovieSearchCard.jsx
+++ src/js/component/MovieSearchCard.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MovieSearchCard = ({title, year, imdbID, poster}) => {
+
+    const [addedFav, setAddedFav] = useState(false);
+
+    function handleAddFav() {
+        setAddedFav(true);
+    }
 
     return (
         <div className='card my-3' style={{width: '18rem'}}>
@@ -9,7 +15,12 @@ const MovieSearchCard = ({title, year, imdbID, poster}) => {
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{`Year: ${year}`} </p>
                 <p className="card-text">{`imdbID: ${imdbID}`} </p>
-                <a href="#" className='btn btn-primary'>Add to Favorites</a>
+                {
+                    addedFav ?
+                    <button disabled={true} className='btn btn-success'>Added to Favorites</button> :
+                    <button className='btn btn-primary' onClick={handleAddFav}>Add to Favorites</button>
+                }
+                
             </div>
         </div>
     );
